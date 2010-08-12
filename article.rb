fa3305d4998f2fdd2e712c23376e7f1356de8584
Benjamin Jørgensen <benjamin@bnjamin.com>
2010-08-13 00:38:37 +0200

    Removed unused blog posts

--- article.rb
+++ article.rb
@@ -5,13 +5,13 @@ class Article
   attr_reader :path, :body
 
   def initialize(path)
-    meta, @body = File.read(path).split(/\n\n/, 2)
-    @yaml = YAML.load(meta)
+    data, @body = File.read(path).split(/\n\n/, 2)
+    @yaml = YAML.load(data)
     @path = 'article/' + File.basename(path, '.txt')
   end
 
   def self.all(path = "articles")
-    Dir["#{path}/*.txt"].reverse.collect { |file| Article.new(file) }
+    Dir["#{path}/*.txt"].reverse.collect { |file| new(file) }
   end
 
   private
